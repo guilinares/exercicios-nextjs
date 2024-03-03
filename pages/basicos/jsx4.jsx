7544ab957da32855026ea99a8db6719ba4235a60
Breuzin <guimine.20@gmail.com>
2024-03-03 20:25:14 -0300

    Integrando JSX com JavaScript

---
+++ pages/basicos/jsx4.jsx
@@ -0,0 +1,18 @@
+export default function jsx4() {
+    const subtitulo = "Estou no JavaScript!"
+    return (
+        <div>
+            <h1>Integração JS e JSX</h1>
+            <h2>{subtitulo}</h2>
+            <h3>{3 * 3}</h3>
+            <h4>{Math.max(13, 39)}</h4>
+            <h4>{entre(9, 1, 10)}</h4>
+        </div>
+    )
+}
+
+function entre(valor, min, max) {
+    if (valor >= min && valor <= max) {
+        return "Sim"
+    } else return "Não"
+}
