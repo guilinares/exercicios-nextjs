0a40622769c40fd5c241418de589a73d4aa1b329
Breuzin <guimine.20@gmail.com>
2024-03-08 21:37:24 -0300

    Usando repetição em tabela

---
+++ data/listaProdutos.js
@@ -0,0 +1,8 @@
+import Produto from "../model/produto";
+
+export default [
+    new Produto(1, "iPad", 7349),
+    new Produto(2, "IPhone", 57803),
+    new Produto(3, "IPhone", 57803),
+    new Produto(4, "Monitor", 1234)
+]
